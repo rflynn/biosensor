419d989b6e5877e4225045954913d0aee06bc351
Ryan Flynn <parseerror@gmail.com>
2016-10-23 23:14:15 -0400

    add convert.sh script; though camera quality is now enforced

---
+++ convert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#set -x
+
+dir=${1:-vid-tag}
+
+du -hcs "$dir"
+
+IFS=$'\n'
+for f in $(find "$dir" -type f -name '*.jpg'); do
+    #echo "$f"
+    quality=$(identify -verbose "$f" | grep Quality | cut -c 12-)
+    if [ $quality -gt 85 ]; then
+        echo "$f"
+        #echo "$quality -gt 85"
+        tmp="${f/%.jpg/-q85.jpg}"
+        #echo "tmp: $tmp"
+        convert -quality 85 "$f" "$tmp" && mv "$tmp" "$f"
+    fi
+done
+
+du -hcs "$dir"
